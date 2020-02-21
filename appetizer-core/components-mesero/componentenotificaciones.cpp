96bd94f95bbbd9cab1d35eb60fdf4d5674857fb7
AngelGenis <38354735+AngelGenis@users.noreply.github.com>
2020-02-20 23:03:25 -0600

    crear estilos de notificaciones

--- appetizer-core/components-mesero/componentenotificaciones.cpp
+++ appetizer-core/components-mesero/componentenotificaciones.cpp
@@ -25,23 +25,37 @@ ComponenteNotificaciones::~ComponenteNotificaciones()
 void ComponenteNotificaciones::MostrarNotificaciones(){
     QSqlQuery query;
 
-    query.exec("SELECT * FROM notificacion"
-               "inner join orden"
-               "on notificacion.orden_id_orden = orden.id_orden"
-               "inner join mesa"
-               "on order.id_mesa = mesa.id_mesa");
+    query.prepare("SELECT * FROM notificacion "
+                  "inner join orden "
+                  "on notificacion.orden_id_orden = orden.id_orden "
+                  "inner join mesa "
+                  "on orden.id_mesa = mesa.id_mesa  where id_mesero = 1 LIMIT 10");
+
+    query.exec();
+
     int row = 0;
     int col = 0;
     int i= 0;
+    qDebug()<<query.exec();
     while(query.next()){
         QString contenido = query.value(1).toString();
         QString mesa = query.value(5).toString();
+        QString imagen = "";
+
+        //Cambiar rutas de acuerdo a la ubicacion en su pc
+
+        if(contenido == "Limpiar Mesa"){
+            imagen = "C:/Users/Angel/Documents/Appetizer/appetizer-core/Img/mesasucia.png";
+        }else if(contenido == "Entregar Platillo"){
+            imagen = "C:/Users/Angel/Documents/Appetizer/appetizer-core/Img/favicon.png";
+        }
 
         row = i/1;
         col= i%1;
 
         NotificacionMesero *noti = new NotificacionMesero();
-        noti->insertarDatos("Numero de mesa: "+ mesa, contenido, "C:/Users/Angel/Documents/Appetizer/appetizer-core/Img/favicon.png");
+
+        noti->insertarDatos("Mesa "+ mesa, contenido, imagen);
         ui->gridLayout->addWidget(noti, row, col);
         i++;
     }
